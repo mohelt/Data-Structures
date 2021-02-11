b7c1371bf5fe055c1de518fc6f041bcdd1d5deb1
mohelt <mohamed.l.eltayeb@gmail.com>
2021-02-11 14:37:48 +0000

    Added Linked Queue (passed all tests)

--- ie.ucd.datastructures/src/LinkedQueue.java
+++ ie.ucd.datastructures/src/LinkedQueue.java
@@ -1,38 +1,51 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LinkedQueue<E> implements Queue<E> {
-
+	
+	private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LinkedQueue<Integer> s = new LinkedQueue<>();
+		for(int i = 0; i < 10; ++i) 
+		{
+			s.enqueue(i);
+		}
+		System.out.println( s.toString());
+	
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return list.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return list.isEmpty();
 	}
 
 	@Override
 	public void enqueue(E e) {
-		// TODO Auto-generated method stub
-		
+		list.addLast(e);
 	}
 
 	@Override
 	public E first() {
 		// TODO Auto-generated method stub
-		return null;
+		return list.first();
 	}
 
 	@Override
 	public E dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		E e = list.first();
+		list.removeFirst();
+		return e;
+	}
+	@Override
+	public String toString() {
+		return list.toString();
 	}
 
 }
